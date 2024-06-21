055c49c9b5d3c44740ae0afaa210aacafec2707c
Clint Decker <clint@tendosystems.com>
2024-06-21 15:05:18 -0700

    Complete deferrable implementation for atlas

--- sql/postgres/inspect.go
+++ sql/postgres/inspect.go
@@ -717,7 +717,7 @@ func (i *inspect) fks(ctx context.Context, s *schema.Schema) error {
 		return fmt.Errorf("postgres: querying schema %q foreign keys: %w", s.Name, err)
 	}
 	defer rows.Close()
-	if err := sqlx.TypedSchemaFKs[*ReferenceOption](s, rows); err != nil {
+	if err := i.addFks(s, rows); err != nil {
 		return fmt.Errorf("postgres: %w", err)
 	}
 	return rows.Err()
